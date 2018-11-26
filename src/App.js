a3a7f563d5aa64e889e679d6da682a2af80be857
Binaya Rimal <rimal.binaya@gmail.com>
2018-11-26 14:47:19 -0800

    as-2 completed

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import * as firebase from 'firebase';
+ import RoomList from './components/RoomList';
+var config = {
+   apiKey: "AIzaSyDNuRFBen5kBfCMVTZWJQn4pRPSIe-kLGw",
+   authDomain: "bloc-chat-439ec.firebaseapp.com",
+   databaseURL: "https://bloc-chat-439ec.firebaseio.com",
+   projectId: "bloc-chat-439ec",
+   storageBucket: "bloc-chat-439ec.appspot.com",
+   messagingSenderId: "322554472388"
+ };
+ firebase.initializeApp(config);
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+
+
+  render(){
+    return(
+      <div>
+    <RoomList firebase= { firebase }  />
+    </div>
+    )
   }
 }
 
